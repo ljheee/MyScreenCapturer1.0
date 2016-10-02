61b75f57bc15bc84f125277ebdcfa4fd82caf0bf
ljheee <3116466300@qq.com>
2016-10-02 20:33:06 +0800

    Add About.

--- src/com/ljheee/snip/MainUI.java
+++ src/com/ljheee/snip/MainUI.java
@@ -12,23 +12,25 @@ import java.awt.image.BufferedImage;
 import javax.swing.*;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+
 /**
  * 初始化--小界面
  * @author ljheee
  *
  */
-public class Main {
+public class MainUI {
 
 	JButton newRectCapture, newFullCapture, cancleBtn, aboutBtn;
 	JFrame jf = new JFrame("\u622A\u56FE\u5DE5\u51771.0");
 	UIListener handler = new UIListener();
 	
-	public Main() {
+	public MainUI() {
 		
 		jf.setLocationRelativeTo(null);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setSize(280, 110);
 		jf.setResizable(false);
+//		jf.setIconImage(new ImageIcon("logo.png").getImage());
 		Image icon = Toolkit.getDefaultToolkit().getImage("/logo.png");
 		jf.setIconImage(icon);
 		
@@ -41,7 +43,7 @@ public class Main {
 		
 		aboutBtn = new JButton();//about.png==35*35px
 //		aboutBtn.setMargin(new Insets(75, 245, 0, 0));
-		aboutBtn.setIcon(new ImageIcon(Main.class.getResource("/about.png")));
+		aboutBtn.setIcon(new ImageIcon(MainUI.class.getResource("/about.png")));
 		aboutBtn.setToolTipText("\u5173\u4E8E");
 		
 		newRectCapture.addActionListener(handler);
@@ -106,7 +108,8 @@ public class Main {
 		public void actionPerformed(ActionEvent e) {
 
 			if (e.getSource() == aboutBtn) {
-				JOptionPane.showMessageDialog(null, "Author:ljheee \nQQ554278334 \n2016-7-23");
+//				JOptionPane.showMessageDialog(null, "Author:ljheee \nQQ554278334 \n2016-7-23");
+				new com.ljheee.snip.ui.about.About(jf,"关于",true);
 			}
 			
 			if (e.getSource() == newRectCapture) {
@@ -141,6 +144,6 @@ public class Main {
 	
 	
 	public static void main(String[] args) {
-		new Main();
+		new MainUI();
 	}
 }
